d6d86e6966c36975bb24ae350fb10ef9e98c42ff
KyleRess-Liere <70179439+KyleRess-Liere@users.noreply.github.com>
2022-12-07 16:49:13 -0500

    update lab1 1 by commnad

--- scripts/local-scripts/Lab1.py
+++ scripts/local-scripts/Lab1.py
@@ -1,67 +1,72 @@
-import json
 import boto3
-import time
 import paramiko
-
-def lambda_handler(event, context):
+import json
+def main():
     ssh_username = "ec2-user"
-    BUCKET_NAME = 'aws-lab-solver'
-    LOCAL_FILE_NAME = '/tmp/keyname.pem'
-    #get address and pem file from the event
-    address = event['address']
-    pemFileLocation = event["pem"]
-    print("address " + address)
-    print("pem "  + pemFileLocation)
+    ssh_key_file = "labsuser.pem"
+    address = "44.204.97.140"
+
+    commands = [
+        "hostname",
+        "aws s3 ls",
+        "aws iam list-users"
     
-    #load the pem file from s3 storage and store in a temporary directory
-    s3 = boto3.client('s3')
-    s3.download_file(BUCKET_NAME, pemFileLocation, LOCAL_FILE_NAME)
-    
-    #start up the ssh session using the pem file and the address
-    k = paramiko.RSAKey.from_private_key_file("/tmp/keyname.pem")
-    #k = paramiko.RSAKey.from_private_key_file(ssh_key_file)
+
+    ]
+
+    k = paramiko.RSAKey.from_private_key_file(ssh_key_file)
     c = paramiko.SSHClient()
     c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     c.connect(hostname= address, username=ssh_username, pkey=k, allow_agent=False, look_for_keys=False)
-    commands = [
-        "hostname",
-        "aws configure",
-        "aws s3 ls",
-        """ echo "Hello World" >> sample.txt """,
-        "aws s3 cp sample.txt s3://samplebucket--beb9beb0",
-        "aws iam list-users",
-        "aws iam list-users --query 'Users[*].UserName'",
-        " aws iam get-user --user-name user-1",
-        " aws iam list-attached-user-policies --user-name user-1",
-        " aws iam list-groups-for-user --user-name user-1" , 
-        " aws iam list-groups",
-        " aws iam get-group --group-name EC2-Support"
-    ]
-    
-    #loops through commands
-    for command in commands:
-        if(command != "aws configure"):
-            print("running command: {}".format(command))
-            stdin , stdout, stderr = c.exec_command(command)
-            time.sleep(1)
-            print(stdout.read().decode('ascii'))
-            print(stderr.read())
-        else:
-            print("running command: {}".format(command))
-            stdin , stdout, stderr = c.exec_command(command)
-            time.sleep(2)
-            stdin.write("AKIAWNBISRYXPQRLSULK")
-            stdin.flush()
-            stdin.write("bItdf12ZJYOfm4+BVEpOGtXL2RotThCc63KglVFU")
-            stdin.flush()
-            stdin.write("us-east-1")
-            stdin.flush()
-            stdin.write("json")
-            stdin.flush()
-    #closes the ssh session
+
+    #hostname
+    command = "hostname"
+    output = runCommand(command,c,k)
+    print(output)
+
+    #aws s3 lis
+    command = "aws s3 ls"
+    output = runCommand(command,c,k)
+   
+    #get the sample bucket id 
+    sampleBucketId = (output).split("--",1)[1]
+    print(sampleBucketId)
+
+    #Create a sample test bucket file 
+    command = """echo "Hello World" >> sample.txt"""
+    output = runCommand(command,c,k)
+
+    #upload the text file to s3 bucket
+    command = "aws s3 cp sample.txt s3://samplebucket--" + sampleBucketId
+    output = runCommand(command,c,k)
+
+    #list the users in the lab
+    command = "aws iam list-users"
+    output = runCommand(command,c,k)
+
+    #query only the usernames
+    command = "aws iam list-users --query 'Users[*].UserName'"
+    output = runCommand(command,c,k)
+
+
+
+
+    #close the parimiko ssh session
+
     c.close()
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+
+
+#################################################################
+#       Input: Command to run , c,k object for ssh              #
+#     Output: The content of what is printed from that command  #
+#################################################################
+def runCommand(command,c,k):
+     print("running command: {}".format(command))
+     stdin , stdout, stderr = c.exec_command(command)
+     output = stdout.read().decode('ascii')
+     print(output)
+     return output
+
+
+main()
