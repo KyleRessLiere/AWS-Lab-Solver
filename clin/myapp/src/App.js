d8a3c098778c01fb5b1aa08ba750043b968d7b29
KyleRessLiere <kressliere@gmail.com>
2022-12-09 19:24:22 -0500

    working POST request to api gateway to initiate lambda

--- clin/myapp/src/App.js
+++ clin/myapp/src/App.js
@@ -9,19 +9,40 @@ import { Storage } from "@aws-amplify/storage";
 import { Auth } from "aws-amplify";
 
 function App() {
+  const labOnePost = async () => {
+    const data = { ssh: "asdasdasdasdasd", pemname: "aasd" };
+    console.log(JSON.stringify(data));
+    fetch(
+      "https://kt5mwprxge.execute-api.us-east-1.amazonaws.com/default/lab1Solver",
+      {
+        method: "POST",
+        header: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((response) => response.text())
+      .then((data) => {
+        console.log("Success:", data);
+      });
+  };
   const createPOST = async () => {
     const post = {
       title: window.prompt("blog post title"),
-      content: window.prompt("blog post content"),
+      content: "post content",
     };
-    await Storage.put("test.txt", "Hello World file");
+    //await Storage.put("test.txt", "Hello World file");
 
+    //calls the lab1 lambda function
+    await labOnePost();
     const newPost = await DataStore.save(new Post(post));
     console.log(newPost);
   };
   return (
     <div className="App">
-      <button onClick={createPOST}>Create POST</button>
+      <button onClick={labOnePost}>Create POST</button>
       <h1>Test</h1>
     </div>
   );
