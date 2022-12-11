b137e587abf68ab0804fee999a8fa03015b36962
KyleRessLiere <kressliere@gmail.com>
2022-12-11 16:46:38 -0500

    form changes for clin

--- clin/myapp/src/App.js
+++ clin/myapp/src/App.js
@@ -3,27 +3,46 @@ import "@aws-amplify/ui-react/styles.css";
 import { DataStore } from "@aws-amplify/datastore";
 import { Lab } from "./models";
 import { Post } from "./models";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Storage } from "@aws-amplify/storage";
 import { Auth } from "aws-amplify";
 
 function App() {
-  const labOnePost = async () => {
+  const [user, setUser] = useState({
+    bastionAddress: "",
+    region: "",
+    user1AccessKey: "",
+    user1SecretKey: "",
+    user2AccessKey: "",
+    user2SecretKey: "",
+    user3AccessKey: "",
+    user3SecretKey: "",
+    labHostId: "",
+  });
+
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    labOnePost(user);
+    console.log("user created", user);
+  };
+  const labOnePost = async (user) => {
     const data = {
-      pemname: "labsuser.pem",
-      address: "44.202.189.244",
-      region: "us-east-1",
-      user1AccessKey: "AKIAWNBISRYXABLY4Z6M",
-      user1SecretKey: "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",
-      user2AccessKey: "AKIAWNBISRYXDS7EITNF",
-      user2SecretKey: "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",
-      user3AccessKey: "AKIAWNBISRYXPBK3DAGI",
-      user3SecretKey: "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",
-      labHostId: "44.202.189.244",
+      pemname: "lab1.pem",
+      address: user.bastionAddress,
+      region: user.region,
+      user1AccessKey: user.user1AccessKey,
+      user1SecretKey: user.user1SecretKey,
+      user2AccessKey: user.user2AccessKey,
+      user2SecretKey: user.user2SecretKey,
+      user3AccessKey: user.user3AccessKey,
+      user3SecretKey: user.user3SecretKey,
+      labHostId: user.labHostId,
     };
 
-    console.log(JSON.stringify(data));
     fetch(
       "https://kt5mwprxge.execute-api.us-east-1.amazonaws.com/default/lab1Solver",
       {
@@ -38,6 +57,13 @@ function App() {
       .then((response) => response.text())
       .then((data) => {
         console.log("Success:", data);
+        const fileData = JSON.stringify(data);
+        const blob = new Blob([fileData], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = "user-info.json";
+        link.href = url;
+        link.click();
       });
   };
 
@@ -66,15 +92,80 @@ function App() {
   };
   return (
     <div className="App">
-      <input type="file" onChange={onChange} />;
-      <button onClick={labOnePost}>Create POST</button>
-      <form>
-        <label>
-          Bastion Host Address:
-          <input type="text" address="Bastion Host Address" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <h1>Aws Lab Solver Lab1</h1>
+
+      <div className="container">
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="labHostId">Lab Host Id</label>
+
+            <input
+              type="text"
+              name="labHostId"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="bastionAddress">Bastion Host</label>
+
+            <input
+              type="text"
+              name="bastionAddress"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="region">Region</label>
+            <input type="text" name="region" onChange={handleChange} required />
+            <label htmlFor="user1AccessKey">user1AccessKey</label>
+            <input
+              type="text"
+              name="user1AccessKey"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="user1SecretKey">user1SecretKey</label>
+            <input
+              type="text"
+              name="user1SecretKey"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="user2AccessKey">user2AccessKey</label>
+            <input
+              type="text"
+              name="user2AccessKey"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="user2SecretKey">user2SecretKey</label>
+            <input
+              type="text"
+              name="user2SecretKey"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="user3AccessKey">user3AccessKey</label>
+            <input
+              type="text"
+              name="user3AccessKey"
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="user3SecretKey">user3SecretKey</label>
+            <input
+              type="text"
+              name="user3SecretKey"
+              onChange={handleChange}
+              required
+            />
+            <hr></hr>
+            <label>Upload .pem file for the lab</label>
+            <input type="file" onChange={onChange} />
+            <div className="submit-btn">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
